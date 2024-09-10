336c01bad317011b5e1c8485b0275513d9731745
Apoorva <apoorvakarekar@gmail.com>
2024-09-10 12:52:21 +0200

    Create operate func that takes an operator and 2 nums
    The function then calls one of the functions
    on the numbers and returns the result

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ const add = (a,b)=> a+b;
 const subtract = (a,b) => a-b;
 const multiply = (a,b) => a*b;
 const divide = (a,b) => a/b;
+
+const container = document.querySelector(".container");
+const firstNum = document.createElement("p");
+// container.appendChild(firstNum);
+const operator = document.createElement("p");
+// container.appendChild(operator);
+const secondNum = document.createElement("p");
+// container.appendChild(secondNum);
+
+
+function operate(operator,num1,num2){
+    switch (operator) {
+        case "+":
+            return add(num1,num2);
+
+        case "-":
+            return subtract(num1,num2);
+
+        case "*":
+            return multiply(num1,num2);
+
+        case "/":
+            return divide(num1,num2);
+
+        default:
+            break;
+    }
+}
