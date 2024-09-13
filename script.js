0dd6cc42016da3867bc702c8f9da182e4b57c7be
Apoorva <apoorvakarekar@gmail.com>
2024-09-13 13:03:02 +0200

    Add switch instead of if..else for different scenarios
    -Using switch case for better readibility
    -Different Switch cases: num,dot,equals,negate

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ function operate(operator,num1,num2){
             return divide(num1,num2);
 
         default:
-            break;
+            return num1+num2;
+            // break;
     }
 }
 
@@ -42,33 +43,63 @@ let num2 = 0;
 
 btns.forEach(button => {
     button.addEventListener('click',(event) => {
-        let eventKey = event.target.textContent;
-        if(eventKey === "clear") {
-            display = "0";
-            num1 = 0;
-            num2 = 0;
+        let eventClassName = event.target.className;
+        let key = event.target.textContent;
+        
+        switch (eventClassName) {
+            case "num":
+                (display === "0") ? display = key : display += key;
+                break;
+
+            case "dot":
+                display.includes(".") ? display : display += key;
+                break;
+
+            case "equals":
+                num2 = Number(display);
+                display = String(operate(oper,num1,num2));
+                // num1 = Number(display);
+                break;
+            
+            case "negate":
+                if (!oper)  display = String(-Number(display));
+                break;
+        
+            default:
+                break;
         }
-        else if(eventKey === ">" && display !== "Bacche ki jaan lega kya!" && display !== "0") 
-            display = display.slice(0,-1);
-        else if(eventKey === "="){
-            num2 = Number(display);
-            display = String(operate(oper,num1,num2));
-            num1 = Number(display);
-        }
-        else if(display.length < 23 && event.target.className !== "oper"){
-            // checks if user has clicked dot
-            ((eventKey === ".") && !(display.includes("."))) ? display += eventKey :
-            //checks if user has clicked a number
-            (!(isNaN(eventKey)) && display !== "0") ?  display += eventKey : 
-            (!(isNaN(eventKey)) && display === "0") ?  display = eventKey :  display;
-        }
-        else if(event.target.className === "oper"){
-            num1 = Number(display);
-            oper = eventKey;
-            display = "";
-        }
-        else 
-            display = "Bacche ki jaan lega kya!";
+
+
+
+        // console.log(eventClassName);
+
+        // let eventKey = event.target.textContent;
+        // if(eventKey === "clear") {
+        //     display = "0";
+        //     num1 = 0;
+        //     num2 = 0;
+        // }
+        // else if(eventKey === ">" && display !== "Bacche ki jaan lega kya!" && display !== "0") 
+        //     display = display.slice(0,-1);
+        // else if(eventKey === "="){
+        //     num2 = Number(display);
+        //     display = String(operate(oper,num1,num2));
+        //     num1 = Number(display);
+        // }
+        // else if(display.length < 23 && event.target.className !== "oper"){
+        //     // checks if user has clicked dot
+        //     ((eventKey === ".") && !(display.includes("."))) ? display += eventKey :
+        //     //checks if user has clicked a number
+        //     (!(isNaN(eventKey)) && display !== "0") ?  display += eventKey : 
+        //     (!(isNaN(eventKey)) && display === "0") ?  display = eventKey :  display;
+        // }
+        // else if(event.target.className === "oper"){
+        //     num1 = Number(display);
+        //     oper = eventKey;
+        //     // display = "";
+        // }
+        // else 
+        //     display = "Bacche ki jaan lega kya!";
         
         savedNum.textContent = display;
     });
