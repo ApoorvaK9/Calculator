41fc197bf5669da68318b9a5ea2c2fbf5a0de8a9
Apoorva <apoorvakarekar@gmail.com>
2024-09-16 10:28:59 +0200

    Evaluate single pair of numbers at a time

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function operate(operator,num1,num2){
         case "-":
             return subtract(num1,num2);
 
-        case "*":
+        case "x":
             return multiply(num1,num2);
 
         case "/":
@@ -34,10 +34,11 @@ function operate(operator,num1,num2){
 }
 
 const btns = document.querySelectorAll("button");
-let savedNum = document.querySelector("#screen p");
-
+let savedNum = document.querySelector(".result");
+// let calculations = document.querySelector(".calculate");
 let display = "0";
-let oper = "";
+let oper1 = "";
+let oper2 = "";
 let num1 = 0;
 let num2 = 0;
 
@@ -49,7 +50,7 @@ btns.forEach(button => {
         switch (eventClassName) {
             case "num":
                 (display.length > 23) ? display = "Bacche ki jaan lega kya!" :
-                (display === "0") ? display = key : display += key;
+                (display === "0" || oper1) ? display = key : display += key;
                 break;
 
             case "dot":
@@ -57,14 +58,24 @@ btns.forEach(button => {
                 display.includes(".") ? display : display += key;
                 break;
 
-            case "equals":
-                num2 = Number(display);
-                display = String(operate(oper,num1,num2));
-                // num1 = Number(display);
+            case "oper":
+                if((!num1 || oper1 === "=") && oper1){
+                    num1 = Number(display);
+                    oper1 = key;
+                }
+                else{
+                    num2 = Number(display);
+                    oper2 = key;
+                    let output = operate(oper1,num1,num2);
+                    oper1 = oper2;
+                    display = output;
+                    num1 = output;
+                    num2 = 0;
+                }
                 break;
 
             case "negate":
-                if (!oper)  display = String(-Number(display));
+                if (!oper1)  display = String(-Number(display));
                 break;
 
             case "clear":
@@ -72,6 +83,8 @@ btns.forEach(button => {
                     display = "0";
                     num1 = 0;
                     num2 = 0;
+                    oper1 = "";
+                    oper2 = "";
                 }
                 else if(display !== "Bacche ki jaan lega kya!"){
                         if((display.length !== 1) && (display !== "0")) 
@@ -79,43 +92,12 @@ btns.forEach(button => {
                         else display = "0";
                     }
                 break;
-                
+
             default:
                 break;
         }
 
-
-        // console.log(eventClassName);
-
-        // let eventKey = event.target.textContent;
-        // if(eventKey === "clear") {
-        //     display = "0";
-        //     num1 = 0;
-        //     num2 = 0;
-        // }
-        // else if(eventKey === ">" && display !== "Bacche ki jaan lega kya!" && display !== "0") 
-        //     display = display.slice(0,-1);
-        // else if(eventKey === "="){
-        //     num2 = Number(display);
-        //     display = String(operate(oper,num1,num2));
-        //     num1 = Number(display);
-        // }
-        // else if(display.length < 23 && event.target.className !== "oper"){
-        //     // checks if user has clicked dot
-        //     ((eventKey === ".") && !(display.includes("."))) ? display += eventKey :
-        //     //checks if user has clicked a number
-        //     (!(isNaN(eventKey)) && display !== "0") ?  display += eventKey : 
-        //     (!(isNaN(eventKey)) && display === "0") ?  display = eventKey :  display;
-        // }
-        // else if(event.target.className === "oper"){
-        //     num1 = Number(display);
-        //     oper = eventKey;
-        //     // display = "";
-        // }
-        // else 
-        //     display = "Bacche ki jaan lega kya!";
-        
         savedNum.textContent = display;
+        // calculations.textContent = `${num1} + ${num2}`;
     });
 });
-
